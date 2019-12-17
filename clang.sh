0af9c26b0ff2cbc597a3dacf35841a745d6aa141
Darren Ramen <darren.ramen@gmail.com>
2019-12-17 16:30:31 +0400

    update github workflow for clang

--- clang.sh
+++ clang.sh
@@ -1,5 +1,5 @@
-PATH="../../clang-r353983c1/bin:../../aarch64-linux-android-4.9/bin:${PATH}" \
-make -j7 O=out \
+PATH="../clang-r353983c1/bin:../aarch64-linux-android-4.9/bin:${PATH}" \
+make -j3 O=out \
                       ARCH=arm64 \
                       CC=clang \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
