fadb694865452bb534660f01fa1ac6f9464a2d3b
Sergey Organov <sorganov@gmail.com>
2020-01-29 14:21:46 +0300

    usb: gadget: serial: fix Tx stall after buffer overflow
    [ Upstream commit e4bfded56cf39b8d02733c1e6ef546b97961e18a ]
    
    Symptom: application opens /dev/ttyGS0 and starts sending (writing) to
    it while either USB cable is not connected, or nobody listens on the
    other side of the cable. If driver circular buffer overflows before
    connection is established, no data will be written to the USB layer
    until/unless /dev/ttyGS0 is closed and re-opened again by the
    application (the latter besides having no means of being notified about
    the event of establishing of the connection.)
    
    Fix: on open and/or connect, kick Tx to flush circular buffer data to
    USB layer.
    
    Signed-off-by: Sergey Organov <sorganov@gmail.com>
    Reviewed-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/gadget/function/u_serial.c
+++ drivers/usb/gadget/function/u_serial.c
@@ -726,12 +726,14 @@ static int gs_start_io(struct gs_port *port)
 	port->n_read = 0;
 	started = gs_start_rx(port);
 
-	/* unblock any pending writes into our circular buffer */
 #ifndef CONFIG_LGE_USB_GADGET
 	if (started) {
 #else
 	if (started && port->port.tty) {
 #endif
+		gs_start_tx(port);
+		/* Unblock any pending writes into our circular buffer, in case
+		 * we didn't in gs_start_tx() */
 		tty_wakeup(port->port.tty);
 	} else {
 		gs_free_requests(ep, head, &port->read_allocated);
