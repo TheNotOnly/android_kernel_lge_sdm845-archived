nathanchance/op6

devfreq_boost: Replace all instances of fb_notif with msm_drm_notif
Also, undo the unnecessary change of blank to a regular integer.

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/devfreq/devfreq_boost.c
@@ -14,13 +14,13 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
-#include <linux/fb.h>
 #include <linux/input.h>
+#include <linux/slab.h>
 #include <linux/msm_drm_notify.h>
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
-	struct notifier_block fb_notif;
+	struct notifier_block msm_drm_notif;
 };
 
 static struct df_boost_drv *df_boost_drv_g __read_mostly;
@@ -249,12 +249,12 @@ static void devfreq_max_unboost(struct work_struct *work)
 	mutex_unlock(&df->lock);
 }
 
-static int fb_notifier_cb(struct notifier_block *nb,
+static int msm_drm_notifier_cb(struct notifier_block *nb,
 	unsigned long action, void *data)
 {
-	struct df_boost_drv *d = container_of(nb, typeof(*d), fb_notif);
+	struct df_boost_drv *d = container_of(nb, typeof(*d), msm_drm_notif);
 	struct msm_drm_notifier *evdata = data;
-	int blank = *(int *)evdata->data;
+	int *blank = evdata->data;
 	bool screen_awake;
 
 	/* Parse framebuffer blank events as soon as they occur */
@@ -262,7 +262,7 @@ static int fb_notifier_cb(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	screen_awake = blank == MSM_DRM_BLANK_UNBLANK_CUST;
+	screen_awake = *blank == MSM_DRM_BLANK_UNBLANK_CUST;
 	devfreq_disable_boosting(d, !screen_awake);
 	if (screen_awake) {
 		int i;
@@ -397,11 +397,11 @@ static int __init devfreq_boost_init(void)
 		goto destroy_wq;
 	}
 
-	d->fb_notif.notifier_call = fb_notifier_cb;
-	d->fb_notif.priority = INT_MAX;
-	ret = msm_drm_register_client(&d->fb_notif);
+	d->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
+	d->msm_drm_notif.priority = INT_MAX;
+	ret = msm_drm_register_client(&d->msm_drm_notif);
 	if (ret) {
-		pr_err("Failed to register fb notifier, err: %d\n", ret);
+		pr_err("Failed to register dsi_panel_notifier, err: %d\n", ret);
 		goto unregister_handler;
 	}
 

TheNotOnly/android_kernel_lge_sdm845-archived

drivers/devfreq/devfreq_boost.c
@@ -14,13 +14,13 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
-#include <linux/fb.h>
 #include <linux/input.h>
+#include <linux/slab.h>
 #include <linux/msm_drm_notify.h>
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
-	struct notifier_block fb_notif;
+	struct notifier_block msm_drm_notif;
 };
 
 static struct df_boost_drv *df_boost_drv_g __read_mostly;
@@ -249,12 +249,12 @@ static void devfreq_max_unboost(struct work_struct *work)
 	mutex_unlock(&df->lock);
 }
 
-static int fb_notifier_cb(struct notifier_block *nb,
+static int msm_drm_notifier_cb(struct notifier_block *nb,
 	unsigned long action, void *data)
 {
-	struct df_boost_drv *d = container_of(nb, typeof(*d), fb_notif);
+	struct df_boost_drv *d = container_of(nb, typeof(*d), msm_drm_notif);
 	struct msm_drm_notifier *evdata = data;
-	int blank = *(int *)evdata->data;
+	int *blank = evdata->data;
 	bool screen_awake;
 
 	/* Parse framebuffer blank events as soon as they occur */
@@ -262,7 +262,7 @@ static int fb_notifier_cb(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	screen_awake = blank == MSM_DRM_BLANK_UNBLANK;
+	screen_awake = *blank == MSM_DRM_BLANK_UNBLANK;
 	devfreq_disable_boosting(d, !screen_awake);
 	if (screen_awake) {
 		int i;
@@ -397,11 +397,11 @@ static int __init devfreq_boost_init(void)
 		goto destroy_wq;
 	}
 
-	d->fb_notif.notifier_call = fb_notifier_cb;
-	d->fb_notif.priority = INT_MAX;
-	ret = msm_drm_register_client(&d->fb_notif);
+	d->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
+	d->msm_drm_notif.priority = INT_MAX;
+	ret = msm_drm_register_client(&d->msm_drm_notif);
 	if (ret) {
-		pr_err("Failed to register fb notifier, err: %d\n", ret);
+		pr_err("Failed to register dsi_panel_notifier, err: %d\n", ret);
 		goto unregister_handler;
 	}
 